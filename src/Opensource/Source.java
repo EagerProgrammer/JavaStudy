af6cb5ffb1e23515deeaec3c936f9713dc91670e
EagerProgrammer <yth960617@unist.ac.kr>
2022-12-27 21:37:38 +0900

    first commit

---
+++ src/Opensource/Source.java
@@ -0,0 +1,7 @@
+package Opensource;
+
+public class Source {
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/Opensource/Test1.java
@@ -0,0 +1,9 @@
+package Opensource;
+
+import junit.framework.TestCase;
+
+public class Test1 extends TestCase {
+    public static void testmethod(){
+        System.out.println(new Source().add(100,200));
+    }
+}
