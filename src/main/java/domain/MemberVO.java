aadb3199341529f0f638aaf0cced5401e185d892
EagerProgrammer <yth960617@unist.ac.kr>
2022-12-30 17:24:24 +0900

    fitst commit

---
+++ src/main/java/domain/MemberVO.java
@@ -0,0 +1,46 @@
+package domain;
+
+public class MemberVO {
+	private String mid;
+	private String mpw;
+	private String mname;
+	private String uuid;
+	
+	
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getMid() {
+		return mid;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemberVO [mid=" + mid + ", mpw=" + mpw + ", mname=" + mname + "]";
+	}
+	
+	
+}
